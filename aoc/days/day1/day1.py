0e7b90882f360e9acf9d24314d0655b1ad2985a5
Salmonwastaken <34370061+Salmonwastaken@users.noreply.github.com>
2024-12-01 13:15:09 +0100

    enable linter (mostly spacing)

--- aoc/days/day1/day1.py
+++ aoc/days/day1/day1.py
@@ -1,38 +1,43 @@
-from aoc.helpers.lineReader import lineReader
 from collections import Counter
 
+from aoc.helpers.lineReader import lineReader
+
+
 def parse_input(content):
-  left_numbers = []
-  right_numbers = []
+    left_numbers = []
+    right_numbers = []
 
-  for line in content:
-    s = line.split()
-    left_numbers.append(int(s[0]))
-    right_numbers.append(int(s[1]))
+    for line in content:
+        s = line.split()
+        left_numbers.append(int(s[0]))
+        right_numbers.append(int(s[1]))
 
-  return left_numbers, right_numbers
+    return left_numbers, right_numbers
 
 
 def part1(left_numbers, right_numbers):
-  left_sorted = sorted(left_numbers)
-  right_sorted = sorted(right_numbers)
+    left_sorted = sorted(left_numbers)
+    right_sorted = sorted(right_numbers)
 
-  total_distance = sum(abs(left - right) for left, right in zip(left_sorted, right_sorted))
-  return total_distance
+    total_distance = sum(
+        abs(left - right) for left, right in zip(left_sorted, right_sorted)
+    )
+    return total_distance
 
 
 def part2(left_numbers, right_numbers):
-  right_counts = Counter(right_numbers)
+    right_counts = Counter(right_numbers)
 
-  similarity_score = sum(v * right_counts[v] for v in left_numbers)
-  return similarity_score
+    similarity_score = sum(v * right_counts[v] for v in left_numbers)
+    return similarity_score
+
 
 if __name__ == "__main__":
-  content = lineReader()
-  left_numbers, right_numbers = parse_input(content)
+    content = lineReader()
+    left_numbers, right_numbers = parse_input(content)
 
-  total_distance = part1(left_numbers, right_numbers)
-  print(f"Total Distance: {total_distance}")
+    total_distance = part1(left_numbers, right_numbers)
+    print(f"Total Distance: {total_distance}")
 
-  similarity_score = part2(left_numbers, right_numbers)
-  print(f"Similarity Score: {similarity_score}")
+    similarity_score = part2(left_numbers, right_numbers)
+    print(f"Similarity Score: {similarity_score}")
--- aoc/helpers/lineReader.py
+++ aoc/helpers/lineReader.py
@@ -1,16 +1,17 @@
 import inspect
 import os
 
+
 def lineReader():
-  caller_frame = inspect.stack()[1]
-  filename = os.path.dirname(caller_frame.filename) + "/input.txt"
+    caller_frame = inspect.stack()[1]
+    filename = os.path.dirname(caller_frame.filename) + "/input.txt"
 
-  try:
-    f = open(filename)
-  except OSError:
-    print(f"Could not open {filename}, exiting")
-    exit()
+    try:
+        f = open(filename)
+    except OSError:
+        print(f"Could not open {filename}, exiting")
+        exit()
 
-  content = f.read().splitlines()
+    content = f.read().splitlines()
 
-  return content
+    return content
