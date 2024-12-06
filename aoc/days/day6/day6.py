206ecb3e23f7e0010b3b31503886cf6838b4bfcb
Salmonwastaken <34370061+Salmonwastaken@users.noreply.github.com>
2024-12-06 10:33:40 +0100

    day6: pre part 2 clean

--- aoc/days/day6/day6.py
+++ aoc/days/day6/day6.py
@@ -9,6 +9,30 @@ def checkBounds(array: list, x: int, y: int) -> bool:
     return 0 <= x < len(array) and 0 <= y < len(array[0])
 
 
+def traverseField(field: list, x: int, y: int) -> set:
+    visited = {(x, y)}
+
+    degrees = 0
+
+    movementHash = {0: (-1, 0), 90: (0, 1), 180: (1, 0), 270: (0, -1)}
+
+    while True:
+        newX = x + movementHash[degrees][0]
+        newY = y + movementHash[degrees][1]
+        if not checkBounds(field, newX, newY):
+            break
+        nextStep = field[newX][newY]
+        if nextStep == "#":
+            degrees = (degrees + 90) % 360
+            continue
+        else:
+            x = newX
+            y = newY
+            visited.add((x, y))
+
+    return visited
+
+
 if __name__ == "__main__":
     content = lineReader(False)
 
@@ -23,27 +47,6 @@ if __name__ == "__main__":
     x = start[0]
     y = start[1]
 
-    visited = {(x, y)}
-
-    degrees = 0
-
-    movementHash = {0: (-1, 0), 90: (0, 1), 180: (1, 0), 270: (0, -1)}
-
-    while True:
-        print(visited)
-        newX = x + movementHash[degrees][0]
-        newY = y + movementHash[degrees][1]
-        if not checkBounds(field, newX, newY):
-            break
-        nextStep = field[newX][newY]
-        if nextStep == "#":
-            print("Invalid Location found!")
-            degrees = (degrees + 90) % 360
-            continue
-        else:
-            print("Valid Location found!")
-            x = newX
-            y = newY
-            visited.add((x, y))
+    visited = traverseField(field, x, y)
 
     print(len(visited))
