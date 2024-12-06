3a847e512765bc309ae314ef2cd128952b8f1b07
Salmonwastaken <34370061+Salmonwastaken@users.noreply.github.com>
2024-12-06 13:43:35 +0100

    day6: cleanup & improvements

--- aoc/days/day6/day6.py
+++ aoc/days/day6/day6.py
@@ -1,16 +1,20 @@
 from aoc.helpers.lineReader import lineReader
-from typing import Union
+from collections import defaultdict
 from copy import deepcopy
+from typing import Union
 
 
+# Build the field grid from the input content
 def buildArray(content: str) -> list:
     return [list(line) for line in content.splitlines()]
 
 
+# Check if the coordinates are within bounds
 def checkBounds(array: list, x: int, y: int) -> bool:
     return 0 <= x < len(array) and 0 <= y < len(array[0])
 
 
+# Find the starting position marked by '^'
 def findStart(field: list) -> tuple:
     for number, row in enumerate(field):
         try:
@@ -19,38 +23,30 @@ def findStart(field: list) -> tuple:
             pass
 
 
+# Traverse the field while checking for obstacles and loop detection
 def traverseField(
     field: list, x: int, y: int, newObstacle: tuple = ()
 ) -> Union[set, bool]:
     visited = {(x, y)}
 
     degrees = 0
-    originalDegrees = degrees
-    originalLocation = (x, y)
-
     movementHash = {0: (-1, 0), 90: (0, 1), 180: (1, 0), 270: (0, -1)}
 
-    hitObstacles = {}
+    hitObstacles = defaultdict(bool)
 
     while True:
-        newX = x + movementHash[degrees][0]
-        newY = y + movementHash[degrees][1]
+        newX, newY = x + movementHash[degrees][0], y + movementHash[degrees][1]
+
         if not checkBounds(field, newX, newY):
             break
 
         nextStep = field[newX][newY]
 
         if nextStep == "#":
-            # If newObstacle is not the default empty tuple, eg we passed a valid one and are parsing part 2
-            # Ugly >:(, what I get for mishmashing it together
             if newObstacle:
-                # Data we need to see if we hit any obstacle for the second time
-                originalDegrees = degrees
-                originalLocation = (x, y)
                 obstacleLocation = (newX, newY)
-                tupleKey = (originalDegrees, originalLocation, obstacleLocation)
-                # Second run in, we've looped
-                if tupleKey in hitObstacles:
+                tupleKey = (degrees, (x, y), obstacleLocation)
+                if hitObstacles[tupleKey]:  # We hit the obstacle again
                     return True
 
                 hitObstacles[tupleKey] = True
@@ -58,40 +54,31 @@ def traverseField(
             degrees = (degrees + 90) % 360
             continue
         else:
-            x = newX
-            y = newY
+            x, y = newX, newY
             visited.add((x, y))
 
-    # very ugly way to mash traversel and loop detection in one
-    if not newObstacle:
-        return visited
-    else:
-        return False
+    return visited if not newObstacle else False
 
 
+# Part 1: Traverse the field and return the number of visited locations
 def part1(field: list, x: int, y: int) -> int:
     visited = traverseField(field, x, y)
-
     return len(visited)
 
 
+# Part 2: Count valid obstacles that block the path without causing loops
 def part2(field: list, x: int, y: int) -> int:
-    # Get original path
     visited = traverseField(field, x, y)
-
-    # For every place we visited, place an obstacle on that square (except the starting position)
     validObstacles = 0
 
     for location in visited:
-        # We can't put an obstacle on top of the guard
         if location == (x, y):
-            print("Skipped initial location")
-            continue
+            continue  # Skip the initial location
 
-        # Copy grid, add blockade to coordinates, traverse again
+        # Create a new field with the obstacle at the current location
         newField = deepcopy(field)
         newField[location[0]][location[1]] = "#"
-        print(f"Traversing Field for {location}")
+
         if traverseField(newField, x, y, (location[0], location[1])):
             validObstacles += 1
 
@@ -100,14 +87,11 @@ def part2(field: list, x: int, y: int) -> int:
 
 if __name__ == "__main__":
     content = lineReader(False)
-
     field = buildArray(content)
-
     start = findStart(field)
-    x = start[0]
-    y = start[1]
+    x, y = start[0], start[1]
 
-    # p1 = part1(field, x, y)
-    # print(f"Part 1: {p1}")
+    p1 = part1(field, x, y)
+    print(f"Part 1: {p1}")
     p2 = part2(field, x, y)
     print(f"Part 2: {p2}")
