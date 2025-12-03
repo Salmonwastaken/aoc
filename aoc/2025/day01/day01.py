abc3677214d064717c9a650348ba4ee4d8e0bf67
Salmonwastaken <34370061+Salmonwastaken@users.noreply.github.com>
2025-12-03 17:09:37 +0100

    Day 1: Delayed Optimization

--- aoc/2025/day01/day01.py
+++ aoc/2025/day01/day01.py
@@ -33,12 +33,30 @@ for rotation in dialtable:
     else:
         op = "+"
 
-    x = 0
-    while x < amount:
-        location = eval(str(location) + op + str(1))
-        location = location % 100
-        if location == 0:
-            answer += 1
-        x += 1
+    # Old method where you just loop over everything, takes 2+ seconds
+    # x = 0
+    # while x < amount:
+    #     location = eval(str(location) + op + str(1))
+    #     location = location % 100
+    #     if location == 0:
+    #         answer += 1
+    #     x += 1
+
+    # Faster method
+
+    # eg 407 becomes 7, we just skip looping 400 times for a predetermined outcome
+    # This makes real input go from 2s to 0.7s
+    roundtrips = amount // 100
+    answer += roundtrips
+    amount -= roundtrips * 100
+
+    # Check wether we start from 0 and if our action crosses the 0 threshold.
+    # This knocks it down to 0.09s
+    end_location = eval(str(location) + op + str(amount))
+    if (end_location <= 0 and location != 0) or (end_location >= 100 and location != 0):
+        last_answer = answer
+        answer += 1
+    location = end_location % 100
+
 
 print(f"Part 2: {answer}")
